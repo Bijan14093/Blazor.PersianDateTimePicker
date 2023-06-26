5a0e40a478d8467e98ec9ae50c5274db10bc8c23
bghasemi <bghasemi@RW80>
2023-06-26 08:50:10 +0330

    first init

---
+++ Blazor.PersianDateTimeControl.UI/Shared/MainLayout.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Blazor.PersianDateTimeControl.UI.Shared
+{
+    public partial class MainLayout: LayoutComponentBase
+    {
+        [Inject]
+        IJSRuntime JS { get; set; }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await JS.InvokeVoidAsync("initPersianCalender");
+            await base.OnAfterRenderAsync(firstRender);
+            }
+    }
+}
