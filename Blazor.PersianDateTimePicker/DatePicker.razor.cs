a7f018cb13e97f31e1a967bfe08144bdbd959fb3
bghasemi <bghasemi@RW80>
2023-06-26 10:27:44 +0330

    1402040510

---
+++ Blazor.PersianDateTimePicker/DatePicker.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.PersianDateTimePicker
+{
+    public partial class DatePicker:ComponentBase
+    {
+        [Inject]
+        IJSRuntime JS { get; set; }
+        //protected override Task OnAfterRenderAsync(bool firstRender)
+        //{
+        //    if (firstRender)
+        //    {
+        //        JS.InvokeVoidAsync("initPersianCalender");
+        //    }
+        //    return base.OnAfterRenderAsync(firstRender);
+        //}
+        protected override void OnAfterRender(bool firstRender)
+        {
+            JS.InvokeVoidAsync("initPersianCalender");
+            base.OnAfterRender(firstRender);
+        }
+    }
+}
